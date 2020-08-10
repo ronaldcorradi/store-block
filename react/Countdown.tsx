85511b0265b1955b373e8ba166e811f1cf3810c0
Ronald Corradi Costa <ronaldcorradi@hotmail.com>
2020-08-10 10:12:36 -0300

    Modificando o bloco countdown para ter estilos configuráveis

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
 
-interface CountdownProps { 
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString();
+
+interface CountdownProps {
   targetDate: string;
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate}) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  tick(targetDate, setTime)
+  
   return (
-  <div>
-    <h1>{targetDate}</h1>
-  </div>
+    <div>
+      <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
+    </div>
   )
 }
 
@@ -17,12 +29,12 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-       targetDate: {
-            title: 'Data final',
-            description: 'Data final utilizada no contador',
-            type: 'string',
-            default: null,
-         },
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
   },
 }
 
