1c1983b1abb7009643076062908dec743fe62110
Ronald Corradi Costa <ronaldcorradi@hotmail.com>
2020-08-10 16:46:05 -0300

    Conectando backend e frontend

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,37 +2,62 @@ import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
 import { useCssHandles } from 'vtex.css-handles'
-import {FormattedMessage} from 'react-intl'
+import { useQuery } from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
 
-const CSS_HANDLES = ["container", "countdown", "title"]
+interface CountdownProps { }
 
-interface CountdownProps {
-  title: string
-  targetDate: string
-}
+const DEFAULT_TARGET_DATE = (new Date('2020-08-08')).toISOString()
+const CSS_HANDLES = ['countdown']
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  title,
-  targetDate,
-}) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ( ) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
-    seconds: '00',
+    seconds: '00'
   })
 
+const productContext = useProduct()
+const product = productContext?.product
+const linkText = product?.linkText
 
-  const titleText = title || <FormattedMessage id="countdown.title" />
+const { data, loading, error } = useQuery(productReleaseDate, {
+  variables: {
+    slug: linkText
+  },
+  ssr: false
+})
+
   const handles = useCssHandles(CSS_HANDLES)
 
-  tick(targetDate, setTime)
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    )
+  }
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
 
   return (
-    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-      <div className={`${handles.title} db tc`}>{titleText}</div>
-      <div className={`${handles.countdown} db tc`}>
-        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-      </div>
+    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
     </div>
   )
 }
@@ -42,11 +67,6 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    title: {
-      title: 'Sou um título',
-      type: 'string',
-      default: null,
-    },
     targetDate: {
       title: 'Data final',
       description: 'Data final utilizada no contador',
